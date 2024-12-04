5dd0e1c79ffe03ad941a9da39c2e5cbded664e97
Vladimir Rudnev <vladimir.rudnev@jetbrains.com>
2024-12-04 12:52:42 +0200

    Day04

---
+++ src/Day04.kt
@@ -0,0 +1,48 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        fun countXmasFrom(i: Int, j: Int): Int {
+            return directions8.count { (di, dj) ->
+                (0 until 4).mapNotNull { step ->
+                    val ni = i + di * step
+                    val nj = j + dj * step
+                    if (ni in input.indices && nj in input[ni].indices) input[ni][nj] else null
+                }.joinToString("") == "XMAS"
+            }
+        }
+        return input.indices.sumOf { i ->
+            input[i].indices.sumOf { j ->
+                countXmasFrom(i, j)
+            }
+        }
+    }
+
+
+    fun part2(input: List<String>): Int {
+        fun isXmasFrom(i: Int, j: Int): Boolean {
+            val diagonals = listOf(
+                listOf(i - 1 to j - 1, i to j, i + 1 to j + 1),
+                listOf(i - 1 to j + 1, i to j, i + 1 to j - 1)
+            )
+            return diagonals.all { diag ->
+                diag.map {
+                    if (it.first !in input.indices || it.second !in input[it.first].indices) return false
+                    input[it.first][it.second]
+                }.joinToString("") in listOf("MAS", "SAM")
+            }
+        }
+        return input.indices.sumOf { i ->
+            input[i].indices.count { j ->
+                isXmasFrom(i, j)
+            }
+        }
+    }
+
+    val testInput = readInput("testInput")
+    check(part1(testInput) == 18) { "wrong part 1! returned ${part1(testInput)}" }
+    val testInput2 = readInput("testInput2")
+    check(part2(testInput2) == 9) { "wrong part 2! returned ${part2(testInput2)}" }
+    val input = readInput("input")
+    part1(input).println()
+    part2(input).println()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,22 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+val directions8 = listOf(
+    Pair(-1, 0),  // up
+    Pair(-1, 1),  // up-right
+    Pair(0, 1),   // right
+    Pair(1, 1),   // down-right
+    Pair(1, 0),   // down
+    Pair(1, -1),  // down-left
+    Pair(0, -1),  // left
+    Pair(-1, -1)  // up-left
+)
+
+val directions4 = listOf(
+    Pair(-1, 0),  // up
+    Pair(0, 1),   // right
+    Pair(1, 0),   // down
+    Pair(0, -1),  // left
+)
